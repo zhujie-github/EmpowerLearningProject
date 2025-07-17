12cd1faf910e01b6105a636af2858ce0a8da500e
zhujie <zhujie198622@gmail.com>
2025-07-17 11:06:23 +0800

    feat:完善仿真平板探测器(None)模块

---
+++ Company.Core/Extensions/ImageExtension.cs
@@ -0,0 +1,69 @@
+﻿using Company.Core.Helpers;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows;
+
+namespace Company.Core.Extensions
+{
+    public static class ImageExtension
+    {
+        /// <summary>
+        /// 将 System.Windows.Media.Imaging.BitmapSource 转换为 System.Drawing.Bitmap
+        /// </summary>
+        /// <param name="bitmapSource"></param>
+        /// <returns></returns>
+        /// <exception cref="NotSupportedException"></exception>
+        public static Bitmap ToBitmap(this System.Windows.Media.Imaging.BitmapSource bitmapSource)
+        {
+            Assert.NotNull(bitmapSource);
+
+            PixelFormat pixelFormat;
+            if (bitmapSource.Format == System.Windows.Media.PixelFormats.Gray8)
+            {
+                pixelFormat = PixelFormat.Format8bppIndexed;
+            }
+            else if (bitmapSource.Format == System.Windows.Media.PixelFormats.Gray16)
+            {
+                pixelFormat = PixelFormat.Format16bppGrayScale;
+            }
+            else if (bitmapSource.Format == System.Windows.Media.PixelFormats.Bgr24)
+            {
+                pixelFormat = PixelFormat.Format24bppRgb;
+            }
+            else if (bitmapSource.Format == System.Windows.Media.PixelFormats.Bgr32)
+            {
+                pixelFormat = PixelFormat.Format32bppArgb;
+            }
+            else
+            {
+                throw new NotSupportedException($"Unsupported pixel format: {bitmapSource.Format}");
+            }
+
+            var bitmap = new Bitmap(bitmapSource.PixelWidth, bitmapSource.PixelHeight, pixelFormat);
+            var bitmapData = bitmap.LockBits(
+                new Rectangle(System.Drawing.Point.Empty, bitmap.Size),
+                ImageLockMode.WriteOnly,
+                pixelFormat);
+            bitmapSource.CopyPixels(
+                Int32Rect.Empty,
+                bitmapData.Scan0,
+                bitmapData.Stride * bitmap.Height,
+                bitmapData.Stride);
+            bitmap.UnlockBits(bitmapData);
+
+            return bitmap;
+        }
+
+        /// <summary>
+        /// 将 System.Drawing.Bitmap 锁定为 BitmapData
+        /// </summary>
+        /// <param name="bitmap"></param>
+        /// <returns></returns>
+        public static BitmapData LockBits(this Bitmap bitmap)
+        {
+            var rect = new Rectangle(System.Drawing.Point.Empty, bitmap.Size);
+            var bitmapData = bitmap.LockBits(rect, ImageLockMode.WriteOnly, bitmap.PixelFormat);
+            return bitmapData;
+        }
+    }
+}
--- Company.Core/Helpers/ImageHelper.cs
+++ Company.Core/Helpers/ImageHelper.cs
@@ -1,7 +1,8 @@
-﻿using Company.Core.Models;
+﻿using Company.Core.Extensions;
+using Company.Core.Models;
 using System.Drawing;
-using System.Drawing.Imaging;
 using System.IO;
+using System.Windows.Media.Imaging;
 
 namespace Company.Core.Helpers
 {
@@ -10,6 +11,12 @@ namespace Company.Core.Helpers
     /// </summary>
     public static class ImageHelper
     {
+        /// <summary>
+        /// 读本地图片转成位图（默认32位）
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <returns></returns>
+        /// <exception cref="FileNotFoundException"></exception>
         public static Bitmap? Load(string filePath)
         {
             if (string.IsNullOrWhiteSpace(filePath) || !File.Exists(filePath))
@@ -24,24 +31,33 @@ namespace Company.Core.Helpers
             }
         }
 
+        /// <summary>
+        /// 读取本地TIFF图片转成无管理数组，当文件（filePath）不存在时，返回null
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <returns></returns>
+        /// <exception cref="FileNotFoundException"></exception>
         public static UnmanagedArray2D<ushort>? LoadTiff(string filePath)
         {
             if (string.IsNullOrWhiteSpace(filePath) || !File.Exists(filePath))
             {
-                throw new FileNotFoundException("TIFF image file not found.", filePath);
-            }
-            var bytes = FileHelper.Load(filePath);
-            using var memoryStream = new MemoryStream(bytes);
-            if (Image.FromStream(memoryStream) is not Bitmap bitmap)
-            {
                 return null;
             }
-            var unmanagedArray = new UnmanagedArray2D<ushort>(bitmap.Width, bitmap.Height);
-            var data = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height),
-                ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
-            MemoryHelper.CopyMemory(unmanagedArray.Header, data.Scan0, unmanagedArray.Length);
-            bitmap.UnlockBits(data);
-            return unmanagedArray;
+
+            var stream = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.Read);
+            var decoder = new TiffBitmapDecoder(stream, BitmapCreateOptions.PreservePixelFormat, BitmapCacheOption.Default);
+            var bitmapSource = decoder.Frames[0];
+            var width = bitmapSource.PixelWidth;
+            var height = bitmapSource.PixelHeight;
+            var stride = width * bitmapSource.Format.BitsPerPixel / 8;
+            var pixels = new ushort[width * height];
+            bitmapSource.CopyPixels(pixels, stride, 0);
+            var bitmap = bitmapSource.ToBitmap();
+            var bitmapData = bitmap.LockBits();
+            var image = new UnmanagedArray2D<ushort>(width, height);
+            MemoryHelper.CopyMemory(image.Header, bitmapData.Scan0, image.Length);
+            bitmap.UnlockBits(bitmapData);
+            return image;
         }
     }
 }
