3a754a30e2e1a60b8940b5f3c8c469272f48ead3
zhujie <zhujie198622@gmail.com>
2025-08-20 16:59:31 +0800

    feat:创建滤波算法模型的接口

--- Company.Application.Flow/ViewModels/AddFilterViewModel.cs
+++ Company.Application.Flow/ViewModels/AddFilterViewModel.cs
@@ -1,8 +1,34 @@
-﻿using Company.Core.Ioc;
+﻿using Company.Application.Share.Filter;
+using Company.Core.Ioc;
+using ReactiveUI;
+using System.Windows.Input;
 
 namespace Company.Application.Flow.ViewModels
 {
     public class AddFilterViewModel : DialogViewModelBase
     {
+        public ICommand SubmitCommand { get; }
+
+        /// <summary>
+        /// 所有算法
+        /// </summary>
+        public List<IFilterModel> DisplayModels { get; }
+
+        /// <summary>
+        /// 当前选中的算法
+        /// </summary>
+        public IFilterModel? DisplayModel { get; set; }
+
+        public AddFilterViewModel()
+        {
+            Title = "选择滤波算法";
+            DisplayModels = []; //todo
+            SubmitCommand = ReactiveCommand.Create(Submit);
+        }
+
+        private void Submit()
+        {
+            //todo
+        }
     }
 }
---
+++ Company.Application.Share/Filter/IFilterModel.cs
@@ -0,0 +1,43 @@
+﻿using Company.Core.Models;
+using System.Reactive;
+
+namespace Company.Application.Share.Filter
+{
+    /// <summary>
+    /// 图像滤波处理算法实体接口，对应OpenCV的图像处理函数
+    /// </summary>
+    public interface IFilterModel
+    {
+        /// <summary>
+        /// 算法名称
+        /// </summary>
+        string Name { get; }
+
+        /// <summary>
+        /// WPF界面名称
+        /// </summary>
+        string View { get; }
+
+        /// <summary>
+        /// 图标
+        /// </summary>
+        string Icon { get; }
+
+        /// <summary>
+        /// 是否启用
+        /// </summary>
+        bool IsEnabled { get; }
+
+        /// <summary>
+        /// 执行当前算法
+        /// </summary>
+        /// <param name="photo"></param>
+        void Filter(UnmanagedArray2D<ushort> photo);
+
+        /// <summary>
+        /// 获取当前算法的观察者
+        /// </summary>
+        /// <returns></returns>
+        IObservable<Unit> GetObservable();
+    }
+}
