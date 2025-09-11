704eb7a0104c39a7737223a239b2a8f0a1e85be6
zhujie <zhujie198622@gmail.com>
2025-09-12 01:41:37 +0800

    feat:增加运动轴的开始移动和停止移动功能

---
+++ Company.Application.Axis/Models/AxisMoveParameter.cs
@@ -0,0 +1,13 @@
+﻿using Company.Hardware.ControlCard;
+
+namespace Company.Application.Axis.Models
+{
+    /// <summary>
+    /// 运动轴控制参数
+    /// </summary>
+    public struct AxisMoveParameter
+    {
+        public AxisType AxisType { get; set; }
+        public MoveDirection Direction { get; set; }
+    }
+}
--- Company.Application.Axis/ViewModels/AxisViewModel.cs
+++ Company.Application.Axis/ViewModels/AxisViewModel.cs
@@ -1,6 +1,61 @@
-﻿namespace Company.Application.Axis.ViewModels
+﻿using Company.Application.Axis.Models;
+using Company.Hardware.ControlCard;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+using System.Windows.Input;
+
+namespace Company.Application.Axis.ViewModels
 {
-    public class AxisViewModel : BindableBase
+    public class AxisViewModel : ReactiveObject
     {
+        private IControlCard ControlCard { get; }
+
+        [Reactive]
+        public bool IsMoving { get; set; }
+
+        /// <summary>
+        /// 开始持续移动某个轴
+        /// </summary>
+        public ICommand StartMoveCommand { get; private set; }
+        /// <summary>
+        /// 结束移动某个轴
+        /// </summary>
+        public ICommand EndMoveCommand { get; private set; }
+        /// <summary>
+        /// 停止所有运动轴
+        /// </summary>
+        public ICommand StopMoveCommand { get; private set; }
+
+        public AxisViewModel(IControlCard controlCard)
+        {
+            ControlCard = controlCard;
+
+            StartMoveCommand = ReactiveCommand.Create<AxisMoveParameter>(StartMove);
+            EndMoveCommand = ReactiveCommand.Create<AxisMoveParameter>(EndMove);
+            StopMoveCommand = ReactiveCommand.Create(StopAllMove);
+        }
+
+        private void StartMove(AxisMoveParameter axisMoveParameter)
+        {
+            IsMoving = ControlCard.Move(axisMoveParameter.AxisType, axisMoveParameter.Direction, out _);
+        }
+
+        private void EndMove(AxisMoveParameter axisMoveParameter)
+        {
+            if (!IsMoving)
+                return;
+
+            ControlCard.Stop(axisMoveParameter.AxisType);
+            IsMoving = false;
+        }
+
+        /// <summary>
+        /// 停止所有运动轴
+        /// </summary>
+        /// <exception cref="NotImplementedException"></exception>
+        private void StopAllMove()
+        {
+            ControlCard.Stop(null);
+        }
     }
 }
